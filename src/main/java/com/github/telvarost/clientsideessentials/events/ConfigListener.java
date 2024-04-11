260eea27791c0dc3e3a6f7669b8a11c091039a56
telontelvarost <telontelvarost@gmail.com>
2024-04-10 21:21:16 -0400

    UDOC-0 - Update change config to reload world so brightness is updated properly

--- src/main/java/com/github/telvarost/clientsideessentials/events/ConfigListener.java
+++ src/main/java/com/github/telvarost/clientsideessentials/events/ConfigListener.java
@@ -1,8 +1,12 @@
 package com.github.telvarost.clientsideessentials.events;
 
 import blue.endless.jankson.JsonObject;
+import net.fabricmc.loader.api.FabricLoader;
 import net.glasslauncher.mods.api.gcapi.api.PreConfigSavedListener;
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.render.TextRenderer;
+import org.lwjgl.input.Mouse;
 
 @EventListener
 public class ConfigListener implements PreConfigSavedListener {
@@ -10,5 +14,11 @@ public class ConfigListener implements PreConfigSavedListener {
     @Override
     public void onPreConfigSaved(int var1, JsonObject jsonObject, JsonObject jsonObject1) {
         /** - Update max stack size on config change */
+        Minecraft minecraft = (Minecraft) FabricLoader.getInstance().getGameInstance();
+        if (null != minecraft) {
+            minecraft.worldRenderer.method_1537();
+            minecraft.textRenderer = new TextRenderer(minecraft.options, "/font/default.png", minecraft.textureManager);
+            //minecraft.textureManager.reloadTexturesFromTexturePack();
+        }
     }
 }
