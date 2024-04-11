01c0a68dba02e3aadaec70d207ca42bf9e79c716
telontelvarost <telontelvarost@gmail.com>
2024-04-11 19:35:09 -0400

    UDOC-0 - Add compatiblity for gambac

--- src/main/java/com/github/telvarost/clientsideessentials/mixin/MinecraftMixin.java
+++ src/main/java/com/github/telvarost/clientsideessentials/mixin/MinecraftMixin.java
@@ -4,7 +4,6 @@ import com.github.telvarost.clientsideessentials.Config;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.io.File;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -25,19 +24,6 @@ public class MinecraftMixin {
 	@Shadow public int actualWidth;
 
 	@Shadow public int actualHeight;
-
-	@Inject(method = "loadSoundFromDir", at = @At("HEAD"))
-	public void clientsideEssentials_loadSoundFromDir(String string, File file, CallbackInfo ci) {
-		Minecraft mc = (Minecraft) (Object) this;
-		int split = string.indexOf("/");
-		String type = string.substring(0, split);
-		String newSound = string.substring(split + 1);
-		// For now only allow the minecart sounds, allowing all of them causes weird effects with same name sounds when the game decides which one to use
-		// XXX Could always incorporate the sound3 part into the sound's name and have it accessible as sound3.random.bow for example, which would avoid the overlap with current sounds
-		if (type.equalsIgnoreCase("sound3") && newSound.startsWith("minecart/")) {
-			mc.soundHelper.addSound(newSound, file);
-		}
-	}
 
 	@Redirect(method = "run", at = @At(value = "INVOKE", target = "Ljava/awt/Canvas;getWidth()I", ordinal = 1), remap = false)
 	public int fixWidth(Canvas canvas){
---
+++ src/main/java/com/github/telvarost/clientsideessentials/mixin/MinecraftSoundMixin.java
@@ -0,0 +1,29 @@
+package com.github.telvarost.clientsideessentials.mixin;
+
+import java.io.File;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.Minecraft;
+
+@Environment(EnvType.CLIENT)
+@Mixin(Minecraft.class)
+public class MinecraftSoundMixin {
+
+    @Inject(method = "loadSoundFromDir", at = @At("HEAD"))
+    public void clientsideEssentials_loadSoundFromDir(String string, File file, CallbackInfo ci) {
+        Minecraft mc = (Minecraft) (Object) this;
+        int split = string.indexOf("/");
+        String type = string.substring(0, split);
+        String newSound = string.substring(split + 1);
+        // For now only allow the minecart sounds, allowing all of them causes weird effects with same name sounds when the game decides which one to use
+        // XXX Could always incorporate the sound3 part into the sound's name and have it accessible as sound3.random.bow for example, which would avoid the overlap with current sounds
+        if (type.equalsIgnoreCase("sound3") && newSound.startsWith("minecart/")) {
+            mc.soundHelper.addSound(newSound, file);
+        }
+    }
+}
