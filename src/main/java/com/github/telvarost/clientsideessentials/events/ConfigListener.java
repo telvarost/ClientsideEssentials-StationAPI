437881d0a54cf96d7d2b19816d2d7f5441b7bc8f
telontelvarost <telontelvarost@gmail.com>
2024-04-11 19:57:03 -0400

    UDOC-0 - Ensure fabric loader is not null and remove old comment

--- src/main/java/com/github/telvarost/clientsideessentials/events/ConfigListener.java
+++ src/main/java/com/github/telvarost/clientsideessentials/events/ConfigListener.java
@@ -14,25 +14,26 @@ public class ConfigListener implements PreConfigSavedListener {
 
     @Override
     public void onPreConfigSaved(int var1, JsonObject jsonObject, JsonObject jsonObject1) {
-        /** - Update max stack size on config change */
-        Minecraft minecraft = (Minecraft) FabricLoader.getInstance().getGameInstance();
-        if (null != minecraft) {
-            boolean brightnessSettingOld = false;
-            boolean brightnessSettingNew = false;
+        if (null != FabricLoader.getInstance()) {
+            Minecraft minecraft = (Minecraft) FabricLoader.getInstance().getGameInstance();
+            if (null != minecraft) {
+                boolean brightnessSettingOld = false;
+                boolean brightnessSettingNew = false;
 
-            minecraft.worldRenderer.method_1537();
-            minecraft.textRenderer = new TextRenderer(minecraft.options, "/font/default.png", minecraft.textureManager);
+                minecraft.worldRenderer.method_1537();
+                minecraft.textRenderer = new TextRenderer(minecraft.options, "/font/default.png", minecraft.textureManager);
 
-            if (null != jsonObject.getObject("BRIGHTNESS_CONFIG")) {
-                brightnessSettingOld = jsonObject.getObject("BRIGHTNESS_CONFIG").getBoolean("ENABLE_BRIGHTNESS_GUI", false);
-            }
+                if (null != jsonObject.getObject("BRIGHTNESS_CONFIG")) {
+                    brightnessSettingOld = jsonObject.getObject("BRIGHTNESS_CONFIG").getBoolean("ENABLE_BRIGHTNESS_GUI", false);
+                }
 
-            if (null != jsonObject1.getObject("BRIGHTNESS_CONFIG")) {
-                brightnessSettingNew = jsonObject1.getObject("BRIGHTNESS_CONFIG").getBoolean("ENABLE_BRIGHTNESS_GUI", false);
-            }
+                if (null != jsonObject1.getObject("BRIGHTNESS_CONFIG")) {
+                    brightnessSettingNew = jsonObject1.getObject("BRIGHTNESS_CONFIG").getBoolean("ENABLE_BRIGHTNESS_GUI", false);
+                }
 
-            if (brightnessSettingOld != brightnessSettingNew) {
-                minecraft.textureManager.reloadTexturesFromTexturePack();
+                if (brightnessSettingOld != brightnessSettingNew) {
+                    minecraft.textureManager.reloadTexturesFromTexturePack();
+                }
             }
         }
     }
