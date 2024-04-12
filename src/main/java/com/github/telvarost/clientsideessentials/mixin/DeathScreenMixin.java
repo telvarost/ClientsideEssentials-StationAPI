127f7ba729c8169bf8970e94769ea318de9477b7
telontelvarost <telontelvarost@gmail.com>
2024-04-11 20:03:26 -0400

    UDOC-0 - Remove even more unused imports

--- src/main/java/com/github/telvarost/clientsideessentials/mixin/DeathScreenMixin.java
+++ src/main/java/com/github/telvarost/clientsideessentials/mixin/DeathScreenMixin.java
@@ -4,17 +4,12 @@ import com.github.telvarost.clientsideessentials.Config;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.ScreenBase;
-import net.minecraft.client.render.TextRenderer;
-import net.minecraft.entity.player.PlayerBase;
-import net.modificationstation.stationapi.api.entity.player.PlayerHelper;
 import net.modificationstation.stationapi.api.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
 import net.minecraft.client.gui.screen.ingame.Death;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Environment(EnvType.CLIENT)
 @Mixin(Death.class)
